3b098a68d50b314e1e4d857003fb6061ac985da1
Thongchai Subsaidee <chanoktrue@me.com>
2021-10-23 22:17:02 +0700

    add next

--- TextFocuStateDemo/ContentView.swift
+++ TextFocuStateDemo/ContentView.swift
@@ -27,6 +27,8 @@ struct ContentView: View {
     
     @FocusState private var focusField: Field?
     
+    @State private var isRegister: Bool = false
+    
     var body: some View {
         
         NavigationView {
@@ -34,49 +36,81 @@ struct ContentView: View {
                 
                 TextField("Firt Name", text: $firstname)
                     .focused($focusField, equals: .firstname)
+                    .submitLabel(.next)
                     .disableAutocorrection(true)
                     .textContentType(.givenName)
                     .keyboardType(.default)
                 
                 TextField("Last Name", text: $lastname)
                     .focused($focusField, equals: .lassname)
+                    .submitLabel(.next)
                     .disableAutocorrection(true)
                     .textContentType(.familyName)
                     .keyboardType(.default)
                 
                 TextField("Email Address", text: $emailaddress)
                     .focused($focusField, equals: .emailaddress)
+                    .submitLabel(.next)
                     .disableAutocorrection(true)
                     .textContentType(.emailAddress)
                     .keyboardType(.emailAddress)
                 
                 TextField("Mobile Number", text: $mobilenumber)
                     .focused($focusField, equals: .mobilenumber)
+                    .submitLabel(.next)
                     .textContentType(.telephoneNumber)
                     .keyboardType(.default)
                 
                 SecureField("Password", text: $password)
                     .focused($focusField, equals: .password)
+                    .submitLabel(.join)
                     .textContentType(.password)
                     .keyboardType(.default)
                 
-                Button {
-                    if firstname.isEmpty {
-                        focusField = .firstname
-                    }else if lastname.isEmpty {
-                        focusField = .lassname
-                    }else if emailaddress.isEmpty {
-                        focusField = .emailaddress
-                    }else if mobilenumber.isEmpty {
-                        focusField = .mobilenumber
-                    }else if password.isEmpty {
-                        focusField = .password
-                    }
-                     
-                } label: {
-                    Text("Create Account")
+                
+                HStack(alignment:.center) {
+                    
                 }
+                
+                if isRegister {
+                    Text("Account Create")
+                        .foregroundColor(.red)
+                        .padding(.vertical)
+                }else {
+                    Button {
+                        if firstname.isEmpty {
+                            focusField = .firstname
+                        }else if lastname.isEmpty {
+                            focusField = .lassname
+                        }else if emailaddress.isEmpty {
+                            focusField = .emailaddress
+                        }else if mobilenumber.isEmpty {
+                            focusField = .mobilenumber
+                        }else if password.isEmpty {
+                            focusField = .password
+                        }else {
+                            isRegister = true
+                        }
+                    } label: {
+                        Text("Create Account")
+                    }
+                }
+                
 
+            }
+            .onSubmit {
+                switch focusField {
+                case .firstname:
+                    focusField = .lassname
+                case .lassname:
+                    focusField = .emailaddress
+                case .emailaddress:
+                    focusField = .mobilenumber
+                case .mobilenumber:
+                    focusField = .password
+                default:
+                    print("Creating account")
+                }
             }
             .navigationBarTitle("@FocusState")
         }
